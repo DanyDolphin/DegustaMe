c5c9fdfb2b6c434a821d86a13b65d4907e9d21b6
GramerOmarFC <grameromar@ciencias.unam.mx>
2022-01-18 21:21:50 -0600

    Pre eliminar forms

--- front/src/app/modules/recetas/pages/agregar-receta/agregar-receta.component.ts
+++ front/src/app/modules/recetas/pages/agregar-receta/agregar-receta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormArray, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-receta',
@@ -7,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AgregarRecetaComponent implements OnInit {
-
+  ingredientes:any= []
   constructor() { }
+  addRecipeForm!: FormGroup;
+
 
   ngOnInit(): void {
-    console.log("aaaaaaaaa")
+    this.ingredientes=[]
+    this.addRecipeForm = new FormGroup({
+      'recipe_name'        : new FormControl('', Validators.required),
+      'recipe_img'         : new FormControl('', [Validators.required]),
+      'recipe_ingredients' : new FormArray([]),
+      'recipe_steps'       : new FormArray([]),
+      'recipe_categories'  : new FormControl('', Validators.required),
+      });
   }
 
+  /* Regresa el objeto FormControl correspondiente al nombre el ingrediente */
+  get name_get() {
+    return this.addRecipeForm.get('recipe_name')!;
+  }
+
+  get img_get() {
+    return this.addRecipeForm.get('recipe_img')!;
+  }
+
+  get get_ingredients_form(){
+    return this.addRecipeForm.get('recipe_ingredients') as FormArray;
+  }
+
+  agregaIngrediente() {
+    let longitud = this.ingredientes.length
+    this.ingredientes.push(2);
+ }
 }
--- front/src/app/modules/recetas/pages/agregar-receta/form-ingrediente/form-ingrediente.component.ts
+++ front/src/app/modules/recetas/pages/agregar-receta/form-ingrediente/form-ingrediente.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-ingrediente',
@@ -8,9 +7,47 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class FormIngredienteComponent implements OnInit {
 
+  // Tipos de medición de cantidades para ingredientes.
+  measurements_types=['piezas','gramos','kilos','onzas','manojos','cucharadas','mililitros','al gusto'];
+  // Tipos de medición de cantidades que no requiere una cantidad numérica.
+  measurements_types_without_amount=['al gusto'];
+
+  // Valores formulario del componente ( nombre, tipo de medición y cantidad del ingrediente en cuestión)
+  ingredient = {
+    name: "",
+    measurement_type: this.measurements_types[0],
+    amount: 0
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  /* Verifica si el tipo de medición recibida requiere o no cantidad numérica */
+  measurement_uses_amount( measurement: string ){
+    return !(this.measurements_types_without_amount.includes(measurement))
+  }
 
+  /* Regresa el nombre del ingrediente */
+  get_ingredient_name(){
+    return this.ingredient.name;
+  }
+
+  /* Regresa la cantidad del ingrediente */
+  get_ingredient_amount(){
+    return this.ingredient.amount;
+  }
+
+  /* Regresa la el tipo de medición del ingrediente */
+  get_ingredient_measurement_type(){
+    return this.ingredient.measurement_type;
+  }
+
+  get datos (){
+    return { "nombre" : this.get_ingredient_name(),
+             "measurement_type": this.get_ingredient_measurement_type(),
+             "amount": this.get_ingredient_amount()
+            }
+  }
 }
