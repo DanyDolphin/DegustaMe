021118799f88fc1cf1b34b70b710ecb35c476df6
GramerOmarFC <grameromar@ciencias.unam.mx>
2022-01-19 03:26:16 -0600

    Front terminado

--- front/src/app/modules/recetas/pages/agregar-receta/agregar-receta.component.ts
+++ front/src/app/modules/recetas/pages/agregar-receta/agregar-receta.component.ts
@@ -8,37 +8,72 @@ import { FormControl, FormArray, FormGroup, Validators, ReactiveFormsModule } fr
 })
 
 export class AgregarRecetaComponent implements OnInit {
-  ingredientes:any= []
   constructor() { }
-  addRecipeForm!: FormGroup;
+
+  // Tipos de medición de cantidades para ingredientes.
+  tiposDeMedicion=['piezas','gramos','kilos','onzas','manojos','cucharadas','mililitros','al gusto'];
+  // Tipos de medición de cantidades que no requiere una cantidad numérica.
+  tiposDeMedicionSinCantidad=['al gusto'];
+
+  // Valores formulario del componente.
+  formAgregaReceta = {
+      nombre: "",
+      imagen: "",
+      ingredientes: new Array<any>(),
+      pasos: new Array<string>(),
+      categorias: ""
+  }
+
+  aceptaTerminos = false;
 
 
   ngOnInit(): void {
-    this.ingredientes=[]
-    this.addRecipeForm = new FormGroup({
-      'recipe_name'        : new FormControl('', Validators.required),
-      'recipe_img'         : new FormControl('', [Validators.required]),
-      'recipe_ingredients' : new FormArray([]),
-      'recipe_steps'       : new FormArray([]),
-      'recipe_categories'  : new FormControl('', Validators.required),
-      });
+    
   }
 
-  /* Regresa el objeto FormControl correspondiente al nombre el ingrediente */
-  get name_get() {
-    return this.addRecipeForm.get('recipe_name')!;
+
+  /* Verifica si el tipo de medición recibida requiere o no cantidad numérica */
+  medicionUsaCantidad( tipoMedicion: string ){
+    return !(this.tiposDeMedicionSinCantidad.includes(tipoMedicion))
   }
 
-  get img_get() {
-    return this.addRecipeForm.get('recipe_img')!;
-  }
-
-  get get_ingredients_form(){
-    return this.addRecipeForm.get('recipe_ingredients') as FormArray;
-  }
-
+  /* Agrega un nuevo campo de ingrediente al formulario de la receta */
   agregaIngrediente() {
-    let longitud = this.ingredientes.length
-    this.ingredientes.push(2);
+    var nuevoIngrediente = { nombre: "", tipo_medicion: this.tiposDeMedicion[0], cantidad: 1 }
+    this.formAgregaReceta.ingredientes.push(nuevoIngrediente);
  }
+
+  /* Elimina un campo de ingrediente del formulario de la receta según el índice recibido */
+  eliminaIngrediente( index: number ){
+    this.formAgregaReceta.ingredientes.splice(index,1);
+  }
+
+  /* Agrega un nuevo campo de ingrediente al formulario de la receta */
+  agregaPaso() {
+    var nuevoPaso = "";
+    this.formAgregaReceta.pasos.push(nuevoPaso);
+ }
+
+  /* Agrega un nuevo campo de ingrediente al formulario de la receta */
+  eliminaPaso() {
+    this.formAgregaReceta.pasos.pop();
+ }
+
+  // Carga la imagen que el usuario selecciona en la ventana de archivos
+  // y la agrega localmente a los datos de imagen del formulario.
+  cargaImagen(event: any) {
+    let imagen = event.target.files[0];
+    if (imagen) {
+      let reader = new FileReader();
+      reader.readAsDataURL(imagen);
+      reader.onload = (e: any) => { this.formAgregaReceta.imagen = e.target.result; }
+    }
+  }
+
+  /* Agrega un nuevo campo de ingrediente al formulario de la receta */
+  eliminaImagen() {
+    this.formAgregaReceta.imagen = "";
+ }
+
+
 }
